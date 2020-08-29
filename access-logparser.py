8811cbd9e4c3347fcba5f4012d2edb63bb08cadc
Michael Ramsey <mike@hackerdise.me>
2020-08-29 10:57:31 +0000

    Update access-logparser.py

--- access-logparser.py
+++ access-logparser.py
@@ -281,7 +281,12 @@ def main():
                 if re.match("(.*)(/admin[a-zA-Z0-9_]*$)(.*)", line):
                     prestashop_hit_count = prestashop_hit_count + 1
                 m = pattern.match(line)
-                hit = m.groupdict()
+                if hit is not None:
+                    hit = m.groupdict()
+                else:
+                    print("re.search() returned None")
+                    continue
+                # hit = m.groupdict()
                 if ispage(hit):
                     pages.append(pythonized(hit))
                 else:
